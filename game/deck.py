8b1de6d2542e7f0030886cd3b1fb98a3533b9808
Yasmin101-101 <yasminmohamudyasmin@gmail.com>
2026-04-08 14:01:48 +0300

    TP-15 work on the deck class

---
+++ game/deck.py
@@ -0,0 +1,27 @@
+from card import Card
+
+class Deck():
+    def __init__(self):
+        ranks = Card.RANKS
+        suites = Card.SUITES
+        deck = []
+        
+        for rank in ranks:
+            for suite in suites:
+                card = Card(suite = suite, rank=rank)
+                deck.append(card)
+        self.deck = deck 
+        
+    def shuffle():
+        pass
+    
+    def remove_card():
+        pass
+    
+    def add_to_end():
+        pass
+    
+if __name__ == "__main__":
+    d1 = Deck()
+    
+    
