78f201fa7e3789386cdfd219cb95129e585d3042
Yasmin101-101 <yasminmohamudyasmin@gmail.com>
2026-04-09 09:32:22 +0300

    TP-17 work on player class

---
+++ game/player.py
@@ -0,0 +1,7 @@
+class Player():
+    def __init__(self, name, type, cards, total_amount_bet, amount):
+        self.name = name
+        self.type = type
+        self.cards = cards
+        self.total_amount_bet = total_amount_bet
+        self.amount = amount
